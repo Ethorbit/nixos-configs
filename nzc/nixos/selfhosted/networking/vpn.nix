783c8df39f0ab925189b0306f5b8439f36345562
Ethorbit <Ethorbit@protonmail.com>
2024-01-14 15:49:08 -0800

    Fix nzc/selfhosted VPN not connecting

--- nzc/nixos/selfhosted/networking/vpn.nix
+++ nzc/nixos/selfhosted/networking/vpn.nix
@@ -1,49 +1,98 @@
 { config, lib, ... }:
 
 {
-    age.secrets."networking/vpn/private.key" = {
-        file = ../secrets/networking/vpn/private.key.age;
-        owner = "systemd-network";
-    };
-    
-    age.secrets."networking/vpn/preshared.key" = {
-        file = ../secrets/networking/vpn/preshared.key.age;
-        owner = "systemd-network";
-    };
- 
-    systemd.network.networks."wg0" = {
-        matchConfig = {
-            Name = "wg0";
-        };
+    options = with lib; {
+        custom.vpn = {
+            ip = mkOption {
+                type = types.str;
+                default = "51.81.202.114";
+            };
 
-        networkConfig = {
-            Address = "10.66.66.2/32";
+            port = mkOption {
+                type = types.str;
+                default = "58300";
+            };
         };
     };
     
-    systemd.network.netdevs = {
-        "wg0" = {
-            enable = true;
-
-            netdevConfig = {
-                Name = "wg0";
-                Kind = "wireguard";
-            };
-
-            wireguardConfig = {
-                PrivateKeyFile = config.age.secrets."networking/vpn/private.key".path;
-                ListenPort = 58300;
-            };
-
-            wireguardPeers = [{
-                wireguardPeerConfig = {
-                    PublicKey = "tJczUpqDMK8LfLgd7PglO0mNsZcow3SS1SxyVncgs2E=";
-                    PresharedKeyFile = config.age.secrets."networking/vpn/preshared.key".path;
-                    AllowedIPs = [ "0.0.0.0/0" ];
-                    Endpoint = "51.81.202.114:58300";
-                    PersistentKeepalive = 25;
+    config = {
+        age.secrets."networking/vpn/private.key" = {
+            file = ../secrets/networking/vpn/private.key.age;
+            owner = "systemd-network";
+        };
+        
+        age.secrets."networking/vpn/preshared.key" = {
+            file = ../secrets/networking/vpn/preshared.key.age;
+            owner = "systemd-network";
+        };
+        
+        systemd.network = {
+            networks = {
+                "eth0" = {
+                    routes = [
+                        {
+                            routeConfig = {
+                                Gateway = "192.168.254.254";
+                                Destination = config.custom.vpn.ip;
+                            };
+                        }
+                    ];
                 };
-            }];
+            
+                "wg0" = {
+                    matchConfig = {
+                        Name = "wg0";
+                    };
+
+                    addresses = [
+                        {
+                            addressConfig = {
+                                Address = "10.66.66.2/24";
+                            };
+                        }
+                    ];
+
+                    routes = [
+                        {
+                            routeConfig = {
+                                Destination = "10.66.66.0/24";
+                            };
+                        }
+                        {
+                            routeConfig = {
+                                Gateway = "10.66.66.1";
+                            };
+                        }
+                    ];
+                };
+            };
+
+            netdevs = {
+                "wg0" = {
+                    enable = true;
+
+                    netdevConfig = {
+                        Name = "wg0";
+                        Kind = "wireguard";
+                        Description = "Wireguard tunnel";
+                    };
+
+                    wireguardConfig = {
+                        PrivateKeyFile = config.age.secrets."networking/vpn/private.key".path;
+                        ListenPort = config.custom.vpn.port;
+                    };
+
+                    wireguardPeers = [{
+                        wireguardPeerConfig = {
+                            PublicKey = "tJczUpqDMK8LfLgd7PglO0mNsZcow3SS1SxyVncgs2E=";
+                            PresharedKeyFile = config.age.secrets."networking/vpn/preshared.key".path;
+                            AllowedIPs = [ "0.0.0.0/0" ];
+                            Endpoint = "${config.custom.vpn.ip}:${config.custom.vpn.port}";
+                            PersistentKeepalive = 25;
+                        };
+                    }];
+                };
+            };
         };
     };
 }
