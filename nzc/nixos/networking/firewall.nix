e7df3d15cf3babd050a00baa76806dc810c635f0
Ethorbit <Ethorbit@protonmail.com>
2024-01-16 23:17:28 -0800

    Fix nzc firewall stacking issue

--- nzc/nixos/networking/firewall.nix
+++ nzc/nixos/networking/firewall.nix
@@ -16,19 +16,21 @@
             iptables -P INPUT DROP
             iptables -P OUTPUT ACCEPT
             iptables -P FORWARD DROP
-            iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-            iptables -A INPUT -i lo -j ACCEPT
-            iptables -A INPUT -i eth0 -m tcp -p tcp --dport ${config.ethorbit.nzc.sshd.port} -j ACCEPT
-            iptables -A INPUT -i eth0 -m tcp -p tcp --dport 53 -j ACCEPT
-            iptables -A INPUT -i eth0 -m udp -p udp --dport 53 -j ACCEPT
-            iptables -A INPUT -i eth0 -s 192.168.254.1/24 -j ACCEPT
-            iptables -A INPUT -s $(cat ${config.age.secrets."networking/firewall/ISP_CIDR_one.age".path}) -j ACCEPT
-            iptables -A INPUT -s $(cat ${config.age.secrets."networking/firewall/ISP_CIDR_two.age".path}) -j ACCEPT
+            iptables -N NIXOS_INPUT
+            [[ ! iptables -C INPUT -j NIXOS_INPUT ]] && iptables -I INPUT 1 -j NIXOS_INPUT
+            iptables -A NIXOS_INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+            iptables -A NIXOS_INPUT -i lo -j ACCEPT
+            iptables -A NIXOS_INPUT -i eth0 -m tcp -p tcp --dport ${config.ethorbit.nzc.sshd.port} -j ACCEPT
+            iptables -A NIXOS_INPUT -i eth0 -m tcp -p tcp --dport 53 -j ACCEPT
+            iptables -A NIXOS_INPUT -i eth0 -m udp -p udp --dport 53 -j ACCEPT
+            iptables -A NIXOS_INPUT -i eth0 -s 192.168.254.1/24 -j ACCEPT
+            iptables -A NIXOS_INPUT -s $(cat ${config.age.secrets."networking/firewall/ISP_CIDR_one.age".path}) -j ACCEPT
+            iptables -A NIXOS_INPUT -s $(cat ${config.age.secrets."networking/firewall/ISP_CIDR_two.age".path}) -j ACCEPT
         '';
         
         extraStopCommands = ''
-            iptables -F
-            iptables -X
+            iptables -P INPUT ACCEPT
+            iptables -D NIXOS_INPUT
         '';
     };
 }
