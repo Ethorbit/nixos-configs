0bb9b483766674f61eaf5c6e065f9a54765d6ef4
Ethorbit <Ethorbit@protonmail.com>
2024-02-05 15:47:28 -0800

    Fix mocp theme

--- nixos/home-manager/moc/default.nix
+++ nixos/home-manager/moc/default.nix
@@ -1,10 +1,13 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
     home-manager.users.${config.ethorbit.users.primary.username} = {
-        home.file.".moc" = {
-            source = ./config;
-            recursive = true;
-        };
+        home.file.".moc/config".text = ''
+            Theme = ${pkgs.moc}/share/moc/themes/transparent-background 
+            Shuffle = yes
+            Autonext = yes
+            MusicDir = $HOME/Music/
+            SoundDriver = PULSEAUDIO
+        '';
     };
 }
