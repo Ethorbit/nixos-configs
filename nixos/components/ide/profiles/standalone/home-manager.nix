3e34acc7e02bc23a2a78c45fae031f266e762692
Ethorbit <Ethorbit@protonmail.com>
2024-02-09 20:48:19 -0800

    Set default browser for ide standalone

--- nixos/components/ide/profiles/standalone/home-manager.nix
+++ nixos/components/ide/profiles/standalone/home-manager.nix
@@ -4,4 +4,15 @@
     imports = [
         ../../../../home-manager/wallpapers/space/4k_1
     ];
+
+    home-manager.users.${config.ethorbit.users.primary.username} = {
+        xdg.mimeApps = {
+            enable = true;
+            defaultApplications = {
+                "text/html" = [ "firefox.desktop" ];
+                "x-scheme-handler/http" = [ "firefox.desktop" ];
+                "x-scheme-handler/https" = [ "firefox.desktop" ];
+            };
+        };
+    };
 }
