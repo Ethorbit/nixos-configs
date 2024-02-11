390e0f97dd535db921d4533ed00afc02d566c86b
Ethorbit <Ethorbit@protonmail.com>
2024-02-11 12:53:51 -0800

    Make xrandr auto res faster for QXL

--- nixos/computer/qemu-vm/services.nix
+++ nixos/computer/qemu-vm/services.nix
@@ -3,25 +3,16 @@
 {
     services.xserver.videoDrivers = [ "qxl" ];
 
-    systemd.user.timers."xrandr-auto-resizer" = {
-        enable = true;
-        description = "Sets QXL resolution with xrandr every 3 seconds.";
-        
-        timerConfig = {
-            OnUnitActiveSec = 3;
-            OnBootSec = 0;
-            AccuracySec = "1us";
-        };
-        
-        wantedBy = [ "timers.target" ];
-    };
-
     systemd.user.services."xrandr-auto-resizer" = {
         enable = true;
         
         serviceConfig = {
             Type = "simple";
             ExecStart = ''${pkgs.xorg.xrandr}/bin/xrandr --output Virtual-1 --auto'';
+            StartLimitInterval = 0.5;
+            StartLimitBurst = 2;
+            Restart = "always";
+            RestartSec = 0.5;
         };
         
         wantedBy = [ "default.target" ];
