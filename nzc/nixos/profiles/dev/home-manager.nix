bb1d1a5797a56838d4b12285f54c4ac7c02fa84c
Ethorbit <Ethorbit@protonmail.com>
2024-02-12 15:22:06 -0800

    Add browsers to nzc dev

--- nzc/nixos/profiles/dev/home-manager.nix
+++ nzc/nixos/profiles/dev/home-manager.nix
@@ -4,4 +4,16 @@
     imports = [
         ../../../../nixos/home-manager/wallpapers/zombies_at_phoneburnia
     ];
+
+    home-manager.users.${config.ethorbit.users.primary.username} = {
+        xdg.mimeApps = {
+            enable = true;
+            defaultApplications = {
+                "text/html" = [ "firefox.desktop" ];
+                "application/xhtml+xml" = [ "firefox.desktop" ];
+                "x-scheme-handler/http" = [ "firefox.desktop" ];
+                "x-scheme-handler/https" = [ "firefox.desktop" ];
+            };
+        };
+    };
 }
--- nzc/nixos/profiles/dev/packages.nix
+++ nzc/nixos/profiles/dev/packages.nix
@@ -3,5 +3,7 @@
 {
     environment.systemPackages = with pkgs; [
         dnsmasq
+        firefox
+        chromium
     ];
 }
