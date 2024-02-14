cbf2c7421f35b6ec4727640033916da0964b1f58
Ethorbit <Ethorbit@protonmail.com>
2024-02-13 19:33:49 -0800

    fix qemu-vm not loading QXL driver

--- nixos/computer/qemu-vm/default.nix
+++ nixos/computer/qemu-vm/default.nix
@@ -2,6 +2,7 @@
 
 {
     imports = [
+        ./services.nix
         ../../components/guest/profiles/qemu
         ../../components/guest/profiles/spice
         ../../components/video-driver/profiles/qxl
--- nixos/computer/qemu-vm/services.nix
+++ nixos/computer/qemu-vm/services.nix
@@ -2,19 +2,4 @@
 
 {
     services.xserver.videoDrivers = [ "qxl" ];
-
-    systemd.user.services."xrandr-virtual-auto-resizer" = {
-        enable = true;
-        
-        serviceConfig = {
-            Type = "simple";
-            ExecStart = ''${pkgs.xorg.xrandr}/bin/xrandr --output Virtual-1 --auto'';
-            StartLimitInterval = 1;
-            StartLimitBurst = 2;
-            Restart = "always";
-            RestartSec = 1;
-        };
-        
-        wantedBy = [ "default.target" ];
-    };
 }
