0ca436e9272601763be38e3121dbc6cfd730bcb9
Ethorbit <Ethorbit@protonmail.com>
2024-02-19 03:07:53 -0800

    Refactor nixpkgs

--- nixos/nixpkgs/default.nix
+++ nixos/nixpkgs/default.nix
@@ -1,9 +1,10 @@
 { inputs, config, ... }:
 
 {
-    nixpkgs.config.allowUnfree = true;
-
-    nixpkgs.overlays = [
-        (import ./overlays/nixpkgs-unstable.nix { inherit inputs; })
-    ];
+    nixpkgs = {
+        config.allowUnfree = true;
+        overlays = [
+            (import ./overlays/nixpkgs-unstable.nix { inherit inputs; })
+        ];
+    };
 }
