bae7b954f168764fcd59991a2eb5f2892dd9ac2f
Ethorbit <Ethorbit@protonmail.com>
2024-04-02 14:20:05 -0700

    Remove sunshine firewall bloat
    If you want to restrict access to a specific CIDR range, you can just do that with your own firewall addition. Because of this, I have removed the support from the sunshine file.

--- nixos/components/desktop-streaming/profiles/sunshine/firewall-rules.nix
+++ nixos/components/desktop-streaming/profiles/sunshine/firewall-rules.nix
@@ -1,25 +1,18 @@
 { config, lib, ... }:
 
 {
-	options = with lib; {
-		ethorbit.components.sunshine.sourceCIDR = mkOption {
-			type = types.str;
-			default = "192.168.0.0/16";
-		};
-	}
-
-	config = lib.mkIf (config.networking.firewall.enable == true) {
-		networking.firewall = {
-			extraCommands = ''
-				iptables -N SUNSHINE
-				iptables -A SUNSHINE -p udp -m multiport --dports 47998,47999,48000,48002 -s ${config.ethorbit.components.sunshine.sourceCIDR} -j ACCEPT
-				iptables -A SUNSHINE -p tcp -m multiport --dports 47984,47989,47990,48010 -s ${config.ethorbit.components.sunshine.sourceCIDR} -j ACCEPT
-				iptables -A INPUT -j SUNSHINE
-			'';
-			
-			extraStopCommands = ''
-				iptables -D SUNSHINE
-			'';
-		};
-	};
+    config = lib.mkIf (config.networking.firewall.enable == true) {
+        networking.firewall = {
+            extraCommands = ''
+                iptables -N SUNSHINE
+                iptables -A SUNSHINE -p udp -m multiport --dports 47998,47999,48000,48002 -j ACCEPT
+                iptables -A SUNSHINE -p tcp -m multiport --dports 47984,47989,47990,48010 -j ACCEPT
+                iptables -A INPUT -j SUNSHINE
+            '';
+            
+            extraStopCommands = ''
+                iptables -D SUNSHINE
+            '';
+        };
+    };
 }
