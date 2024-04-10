a089ea87db79b7af0e58d04cfd2138717adeed32
Ethorbit <Ethorbit@protonmail.com>
2024-04-09 23:57:40 -0700

    Add missing hyper-v support

--- nixos/hardware/vm/hyperv/default.nix
+++ nixos/hardware/vm/hyperv/default.nix
@@ -1,5 +1,10 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
-	virtualisation.hypervGuest.enable = true;
+    virtualisation.hypervGuest.enable = true;
+
+    environment.systemPackages = with pkgs; [
+        linuxKernel.packages.linux_6_1.vm-tools
+        linuxKernel.packages.linux_6_1.hyperv-daemons
+    ];
 }
