b3d8bd4fab462c62d7c995bfab79b6d938055d03
Ethorbit <Ethorbit@protonmail.com>
2024-04-14 23:02:54 -0700

    Workstation, restrict root again

--- workstation/nixos/host-and-containers/users.nix
+++ workstation/nixos/host-and-containers/users.nix
@@ -9,9 +9,9 @@
         # There's no reason for root; workstation user uses sudo
         # and containers don't use any form of root for security.
         users."root" = {
-            hashedPassword = "$6$4.rqF26SKsvG2HcV$9fdNNRHTu8FdsfiZzWOuhY8laaS5Fts42cXiqFlQJsPl4hG2HgoJA98CCgthPPwj1x5tjkFvh9kHMtotuu4t4.";
-            #shell = ''${pkgs.shadow}/bin/nologin'';
-            #hashedPassword = "!";
+            #hashedPassword = "$6$4.rqF26SKsvG2HcV$9fdNNRHTu8FdsfiZzWOuhY8laaS5Fts42cXiqFlQJsPl4hG2HgoJA98CCgthPPwj1x5tjkFvh9kHMtotuu4t4.";
+            shell = ''${pkgs.shadow}/bin/nologin'';
+            hashedPassword = "!";
         };
     };
 }
