e0666494bf717d623b350e1e2308ff906bc68408
Ethorbit <Ethorbit@protonmail.com>
2024-04-15 07:56:50 -0700

    Add workstation container users to power group

--- workstation/nixos/containers/config/users.nix
+++ workstation/nixos/containers/config/users.nix
@@ -13,6 +13,7 @@
                 linger = true;
                 isNormalUser = true;
                 group = "container";
+                extraGroups = [ "power" ];
                 uid = 1000 + config.ethorbit.workstation.xorg.sessionNumbers.${config.ethorbit.users.primary.username};
                 hashedPasswordFile = config.age.secrets."user/password".path;
                 openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/m67X4bZrhN86eFAAp3RGEzhzUp0k1WAP7dw31fAVS ethorbit@nixos" ];
