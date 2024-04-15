c1986caeaa7b057a7ee6e10eb9fe1606630851e3
Ethorbit <Ethorbit@protonmail.com>
2024-04-15 07:58:06 -0700

    Remove /bin/nologin shell for workstation root user
    This change allows nixos-container root-login to succeed, allowing secure access to container root user when maintenance is needed.

--- workstation/nixos/host-and-containers/users.nix
+++ workstation/nixos/host-and-containers/users.nix
@@ -6,11 +6,9 @@
 
         groups."container".gid = 1001;
 
-        # There's no reason for root; workstation user uses sudo
-        # and containers don't use any form of root for security.
+        # workstation uses sudo and can do nixos-container root-login 
+        # to access root on containers
         users."root" = {
-            #hashedPassword = "$6$4.rqF26SKsvG2HcV$9fdNNRHTu8FdsfiZzWOuhY8laaS5Fts42cXiqFlQJsPl4hG2HgoJA98CCgthPPwj1x5tjkFvh9kHMtotuu4t4.";
-            shell = ''${pkgs.shadow}/bin/nologin'';
             hashedPassword = "!";
         };
     };
