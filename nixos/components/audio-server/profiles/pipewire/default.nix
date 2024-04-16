323ec1cb18085bdf85c1627811eb752226fe6fd1
Ethorbit <Ethorbit@protonmail.com>
2024-04-16 02:10:39 -0700

    Enable alsa support for pipewire component

--- nixos/components/audio-server/profiles/pipewire/default.nix
+++ nixos/components/audio-server/profiles/pipewire/default.nix
@@ -8,6 +8,10 @@
 
     services.pipewire = {
         enable = true;
+        alsa = {
+            enable = true;
+            support32Bit = true;
+        };
         pulse.enable = true;
     };
 
