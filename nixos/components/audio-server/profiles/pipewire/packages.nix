b467581b9b9e460d01062755df460b51a0003df2
Ethorbit <Ethorbit@protonmail.com>
2024-04-16 00:24:47 -0700

    Add pulseaudio package to pipewire

--- nixos/components/audio-server/profiles/pipewire/packages.nix
+++ nixos/components/audio-server/profiles/pipewire/packages.nix
@@ -4,5 +4,11 @@
     environment.systemPackages = with pkgs; [
         pipewire
         pavucontrol
+        
+        # Since pulse support will be enabled
+        # pulseaudio commands will be compatible
+        # but to use them pulseaudio needs to
+        # be installed too.
+        pulseaudio
     ];
 }
