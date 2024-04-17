43d3d954a09c0bd49f7578901efe9b8600a4848c
Ethorbit <Ethorbit@protonmail.com>
2024-04-17 09:16:53 -0700

    Add container to extra groups

--- workstation/nixos/containers/config/users.nix
+++ workstation/nixos/containers/config/users.nix
@@ -5,7 +5,10 @@
 
     systemd.tmpfiles.rules = [
       "f /var/lib/systemd/linger/${config.ethorbit.users.primary.username}"
+      "d /etc/opt/VirtualGL 750 root vglusers -"
     ];
+
+    users.groups.vglusers = {};
 
     users = {
         users = {
@@ -13,7 +16,7 @@
                 linger = true;
                 isNormalUser = true;
                 group = "container";
-                extraGroups = [ "power" ];
+                extraGroups = [ "video" "power" "vglusers" ];
                 uid = 1000 + config.ethorbit.workstation.xorg.sessionNumbers.${config.ethorbit.users.primary.username};
                 hashedPasswordFile = config.age.secrets."user/password".path;
                 openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/m67X4bZrhN86eFAAp3RGEzhzUp0k1WAP7dw31fAVS ethorbit@nixos" ];
