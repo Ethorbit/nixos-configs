84bda82a55c95d5160f50ea2706e60ba8b5407bd
Ethorbit <Ethorbit@protonmail.com>
2024-04-17 09:17:38 -0700

    Add virtualgl packages to workstation containers
    It doesn't really do anything, but maybe I am just using it wrong \o/

--- workstation/nixos/containers/config/packages.nix
+++ workstation/nixos/containers/config/packages.nix
@@ -2,8 +2,14 @@
 
 {
     environment.systemPackages = with pkgs; [
-        #linuxKernel.packages.linux_6_1.nvidia_x11_production
-        xorg.libXtst
-        xorg.xeyes
+        # This was for a Sunshine attempt
+        ##linuxKernel.packages.linux_6_1.nvidia_x11_production
+        #xorg.libXtst
+        #xorg.xeyes
+
+        # (following morrolinux's guide for GPU accelerated container)
+        libGL
+        mesa
+        virtualgl
     ];
 }
