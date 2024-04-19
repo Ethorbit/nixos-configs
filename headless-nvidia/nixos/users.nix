1b0819eeecb74ebb7d44b0c2bb0900f25c067554
Ethorbit <Ethorbit@protonmail.com>
2024-04-19 10:40:15 -0700

    Refactor all users

--- headless-nvidia/nixos/users.nix
+++ headless-nvidia/nixos/users.nix
@@ -3,7 +3,6 @@
 {
     ethorbit.users.primary.username = "nvidia";
     users = {
-        groups.${config.ethorbit.users.primary.username}.gid = "1000";
         users.${config.ethorbit.users.primary.username} = {
             extraGroups = [ "wheel" "docker" ];
         };
