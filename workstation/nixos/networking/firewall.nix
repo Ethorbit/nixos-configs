6c871a97c9a99c47062c980f7bcc237fe785920f
Ethorbit <Ethorbit@protonmail.com>
2024-04-27 21:34:53 -0700

    Update workstation firewall

--- workstation/nixos/networking/firewall.nix
+++ workstation/nixos/networking/firewall.nix
@@ -3,7 +3,9 @@
 {
     networking.firewall = {
         extraCommands = ''
-            iptables -I INPUT 1 -s ${config.ethorbit.network.admin.ip} -j ACCEPT
+            iptables -P INPUT DROP
+            iptables -I nixos-fw 1 -s ${config.ethorbit.network.admin.ip} -j nixos-fw-accept
+            iptables -A nixos-fw -s 172.0.0.0/24 -j nixos-fw-accept
         '';
     };
 }
