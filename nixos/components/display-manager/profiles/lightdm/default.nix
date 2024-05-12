dda3cbbebda0586054dde5e2ff76c2315d7b5bc7
Ethorbit <Ethorbit@protonmail.com>
2024-05-11 22:11:10 -0700

    Add lightlocker for lightdm component

--- nixos/components/display-manager/profiles/lightdm/default.nix
+++ nixos/components/display-manager/profiles/lightdm/default.nix
@@ -1,8 +1,12 @@
-{ config, lib, ... }:
+{ config, pkgs, lib, ... }:
 
 {
     services.xserver.displayManager.lightdm = {
         enable = lib.mkDefault true;
         greeters.gtk.enable = lib.mkDefault true;
     };
+
+    environment.systemPackages = with pkgs; [
+        lightlocker
+    ];
 }
