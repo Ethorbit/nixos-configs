000ac2a1733bd2b6bf95d801ca6c1e43a1e2e150
Ethorbit <Ethorbit@protonmail.com>
2024-05-16 02:32:01 -0700

    Fix PS1 implementation

--- nixos/home-manager/bash/default.nix
+++ nixos/home-manager/bash/default.nix
@@ -5,8 +5,8 @@
         programs.bash = {
             enable = true;
             bashrcExtra = ''
-                # Change terminal font color from green to darkish purple (fits dark theming better)
-                export PS1='\[\e[36m\]\u@\h\[\e[0m\]:\w\''$ ';
+                # Change terminal username@host color from default green to darkish purple (fits dark theming better)
+                export PS1='\[\e[36m\]\u@\h\[\e[0m\]:\w\''$ '
 
                 [ -f ~/.bashrc_aliases ] && source ~/.bashrc_aliases
             '';
