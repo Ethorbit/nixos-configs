2c89736e3f862720f19279ae3425f5763dd73972
Ethorbit <Ethorbit@protonmail.com>
2024-05-16 02:29:14 -0700

    Change PS1

--- nixos/home-manager/bash/default.nix
+++ nixos/home-manager/bash/default.nix
@@ -5,6 +5,9 @@
         programs.bash = {
             enable = true;
             bashrcExtra = ''
+                # Change terminal font color from green to darkish purple (fits dark theming better)
+                export PS1='\[\e[36m\]\u@\h\[\e[0m\]:\w\''$ ';
+
                 [ -f ~/.bashrc_aliases ] && source ~/.bashrc_aliases
             '';
         };
