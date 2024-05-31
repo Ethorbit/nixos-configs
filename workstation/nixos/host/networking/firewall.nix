9d2480a6ea424eebe5006e46caf3b12d5f3e052b
Ethorbit <Ethorbit@protonmail.com>
2024-05-30 20:38:23 -0700

    Add workstation firewall rules for containers

--- workstation/nixos/host/networking/firewall.nix
+++ workstation/nixos/host/networking/firewall.nix
@@ -1,14 +1,38 @@
-{ config, ... }:
+{ config, lib, ... }:
 
+with lib;
+let
+    containerIPs = map (name: "${config.ethorbit.workstation.containers.${name}.ip}") (attrNames config.ethorbit.workstation.containers);
+in
 {
     networking.firewall = {
         enable = true;
-        allowedTCPPorts = [ 22 80 443 4713 ];
+        allowedTCPPorts = [ 22 80 443 4713 ]; # SSH, Traefik, Pulseaudio
+        
+    # The fake exit code 0s are due to the custom chain commands exiting with error code 1 even though they succeed.
+    # I have no idea why they do this and it causes the firewall service to fail, so the fake exit 0 is my workaround.
+    # Everything seems to get added fine now. \o/
         extraCommands = ''
+            iptables -N INPUT_CONTAINERS 2> /dev/null || (exit 0)
             iptables -P INPUT DROP
+            iptables -P INPUT_CONTAINERS DROP 2> /dev/null || (exit 0)
+
+            # By default, SSH is allowed by any system. We only need the central admin system to SSH.
             iptables -I INPUT 1 -p tcp -m tcp --dport 22 ! -s ${config.ethorbit.network.admin.ip} -j DROP
+
+            # Send incoming container connections to the container chain
+            ${toString (map (ip: ''
+            iptables -I INPUT 2 -s ${ip} -j INPUT_CONTAINERS
+            '') containerIPs)}
+
+            # Allow containers to connect to host's COTURN server.
+            iptables -A INPUT_CONTAINERS -p tcp -m multiport --dports 3478,3479,49160:49200 -j ACCEPT
+            iptables -A INPUT_CONTAINERS -p udp -m multiport --dports 3478,3479,49160:49200 -j ACCEPT
         '';
+
         extraStopCommands = ''
+            iptables -P INPUT_CONTAINERS ACCEPT 2> /dev/null || (exit 0)
+            iptables -F INPUT_CONTAINERS 2> /dev/null || (exit 0)
             iptables -P INPUT ACCEPT
             iptables -F INPUT
         '';
