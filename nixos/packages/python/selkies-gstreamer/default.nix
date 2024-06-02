765b539aff44d77139fd0745492fedf9a8974a5d
Ethorbit <Ethorbit@protonmail.com>
2024-06-01 21:21:17 -0700

    Add selkies gstreamer gst-web component

--- nixos/packages/python/selkies-gstreamer/default.nix
+++ nixos/packages/python/selkies-gstreamer/default.nix
@@ -2,6 +2,10 @@
 
 with pkgs;
 with lib;
+
+let
+    version = "1.5.2";
+in
 {
     imports = [
         ../gputil
@@ -10,15 +14,31 @@ with lib;
     ];
 
     options = {
+        ethorbit.pkgs.python.selkies-gstreamer-web = mkOption {
+            type = types.package;
+            default = (stdenv.mkDerivation {
+                name = "selkies-gstreamer-web";
+                description = "The HTML5/JS web components for Selkies-Gstreamer.";
+                src = fetchurl {
+                    url = "https://github.com/selkies-project/selkies-gstreamer/releases/download/v${version}/selkies-gstreamer-web-v${version}.tgz";
+                    sha256 = "sha256-DgJwANQ/ritGYPoYaNo4STbrvHCvvdyX8yz67QGDA6Y=";
+                };
+                installPhase = ''
+                    mkdir -p $out
+                    tar -xzf $src -C $out/
+                '';
+           });
+        };
+
         ethorbit.pkgs.python.selkies-gstreamer = mkOption {
             type = types.package;
             default = (python3Packages.buildPythonPackage {
                 pname = "selkies-gstreamer";
-                version = "1.5.2";
+                version = "${version}";
                 format = "wheel";
                 description = "Open-Source Low-Latency Linux WebRTC HTML5 Remote Desktop and 3D Graphics / Game Streaming Platform with GStreamer";
                 src = fetchurl {
-                    url = "https://github.com/selkies-project/selkies-gstreamer/releases/download/v1.5.2/selkies_gstreamer-1.5.2-py3-none-any.whl";
+                    url = "https://github.com/selkies-project/selkies-gstreamer/releases/download/v${version}/selkies_gstreamer-${version}-py3-none-any.whl";
                     sha256 = "sha256-OO3b9pjljZO2t5ITuXnK4yPSnJghQlN+OouM0BQ9a8U=";
                 };
                 nativeBuildInputs = [
@@ -47,10 +67,10 @@ with lib;
                     gst_all_1.gst-plugins-ugly
                     gst_all_1.gst-plugins-good
                 ];
-                # so that it stops crying with runtime errors [\/]
                 preFixup = ''
                     for f in $(find $out/bin/ -type f -executable); do
                         wrapProgram "$f" \
+                          --prefix WEB_ROOT ":" "${config.ethorbit.pkgs.python.selkies-gstreamer-web}/gst-web" \
                           --prefix GI_TYPELIB_PATH ":" "${gobject-introspection.out}/lib/girepository-1.0:$GI_TYPELIB_PATH" \
                           --prefix GST_PY_PATH ":" "${python311Packages.gst-python}/lib/python3.11" \
                           --prefix GST_PLUGIN_SYSTEM_PATH_1_0 ":" "${config.ethorbit.pkgs.libnice}/lib/gstreamer-1.0:${gst_all_1.gstreamer}/lib/gstreamer-1.0:${gst_all_1.gst-plugins-base}/lib/gstreamer-1.0:${gst_all_1.gst-plugins-good}/lib/gstreamer-1.0:${gst_all_1.gst-plugins-bad}/lib/gstreamer-1.0:${gst_all_1.gst-plugins-ugly}/lib/gstreamer-1.0" \
