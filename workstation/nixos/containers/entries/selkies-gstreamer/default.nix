8b91f10e89045b576b2c2ff021d1095aa0c3b0fb
Ethorbit <Ethorbit@protonmail.com>
2024-06-05 01:09:46 -0700

    Make Traefik containers use domain instead of path
    As much as I wanted to avoid using a domain name, it seems that Reverse Proxy paths cause a lot of website pathing issues that don't occur when you use domain names. In this case, Selkies-Gstreamer couldn't load files by relative path because Traefik with its PathPrefix was causing relative paths to be treated as absolute paths. Every forum post I read said to basically use a domain name instead, so here we are..

--- workstation/nixos/containers/entries/selkies-gstreamer/default.nix
+++ workstation/nixos/containers/entries/selkies-gstreamer/default.nix
@@ -2,57 +2,35 @@
 
 let
     # This will allow clients outside host network to be able to connect to the desktops.
-    # https://<host>/container/<name>
+    # https://container.<container name>.<hostname>.internal
+    # clients must have *.<hostname>.internal route to host's public IP for connection to work
     traefikCreator = name: entry: {
         http = {
-            # The web app listens on the trailing slash, so it is
-            # important to enforce it, or else the content won't load
-            #
-            # We also enforce HTTPS since it is required for some interface
+            # We enforce HTTPS since it is required for some interface
             # features, such as the clipboard sharing.
-            routers."${name}-http-no-trailing-slash" = {
-                rule = "PathPrefix(`/container/${name}`)";
-                service = "${name}";
-                entrypoints = "web";
-                middlewares = [ "trailing-slash-redirect-http" "https-redirect" ];
-            };
-
-            routers."${name}-https-no-trailing-slash" = {
-                rule = "PathPrefix(`/container/${name}`)";
-                tls = true;
-                service = "${name}";
-                entrypoints = "websecure";
-                middlewares = [ "trailing-slash-redirect-https" ];
-            };
-
             routers."${name}-http" = {
-                rule = "PathPrefix(`/container/${name}/`)";
+                rule = "Host(`container.${name}.${config.networking.hostName}.internal`)";
                 service = "${name}";
                 entrypoints = "web";
                 middlewares = [ "https-redirect" ];
             };
 
             routers."${name}-https" = {
-                rule = "PathPrefix(`/container/${name}/`)";
+                rule = "Host(`container.${name}.${config.networking.hostName}.internal`)";
                 tls = true;
                 service = "${name}";
                 entrypoints = "websecure";
-                middlewares = [ "${name}-strip-prefix" ];
             };
 
-            services."${name}" = {
-                loadBalancer = {
-                    servers = [
-                        {
-                            url = "http://${entry.ip}:8080";
-                        }
-                    ];
-                };
-            };
-
-            middlewares = {
-                "${name}-strip-prefix" = {
-                    stripPrefix.prefixes = "/container/${name}/";
+            services = {
+                "${name}" = {
+                    loadBalancer = {
+                        servers = [
+                            {
+                                url = "http://${entry.ip}:8080/";
+                            }
+                        ];
+                    };
                 };
             };
         };
