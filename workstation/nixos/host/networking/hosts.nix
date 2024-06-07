2f2fd44634cdfd1615dbe0798e3c501bb1a210d7
Ethorbit <Ethorbit@protonmail.com>
2024-06-07 00:24:11 -0700

    Add network entry 'host' to host.
    It's not really necessary, but might help when containers reference host as their entry connects to the public IP.

--- workstation/nixos/host/networking/hosts.nix
+++ workstation/nixos/host/networking/hosts.nix
@@ -4,8 +4,13 @@ with lib;
 
 {
     # Add host entries for each container
-    networking.hosts = (mkMerge (map (name: {
-        "127.0.0.1" = [ "container.${name}.${config.networking.hostName}.internal" ];
-        "${config.ethorbit.workstation.containers.entries."${name}".ip}" = [ "container.${name}.internal" ];
-    }) (attrNames config.ethorbit.workstation.containers.entries)));
+    networking.hosts = mkMerge [ 
+        {
+            "127.0.0.1" = [ "host" ];
+        } 
+        (mkMerge (map (name: {
+            "127.0.0.1" = [ "container.${name}.${config.networking.hostName}.internal" ];
+            "${config.ethorbit.workstation.containers.entries."${name}".ip}" = [ "container.${name}.internal" ];
+        }) (attrNames config.ethorbit.workstation.containers.entries))) 
+    ];
 }
