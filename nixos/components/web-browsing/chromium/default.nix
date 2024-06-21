ffee89e75927af61bbc2faf3780a7870a8926d88
Ethorbit <Ethorbit@protonmail.com>
2024-06-21 15:43:29 -0700

    Add settings for chromium component

--- nixos/components/web-browsing/chromium/default.nix
+++ nixos/components/web-browsing/chromium/default.nix
@@ -3,6 +3,28 @@
 {
     programs.chromium = {
         enable = true;
+
+        extraOpts = {
+            "BrowserSignin" = 0;
+            "SyncDisabled" = true;
+            "PasswordManagerEnabled" = false;
+            "PasswordSharingEnabled" = false;
+            "SpellcheckEnabled" = true;
+            "SpellcheckLanguage" = [
+                "en-US"
+            ];
+            "DefaultBrowserSettingEnabled" = false;
+            "DefaultGeolocationSetting" = 2;
+            "DnsOverHttpsMode" = "secure";
+            "HttpsOnlyMode" = "force_enabled";
+            "DefaultSearchProviderSearchURL" = "https://duckduckgo.com/?q={searchTerms}";
+            "AutofillAddressEnabled" = false;
+            "AutoFillEnabled" = false;
+            "ImportAutofillFormData" = false;
+            "ImportSavedPasswords" = false;
+            "ImportHistory" = false;
+        };
+
         extensions = lib.mkDefault [
             # Ublock Origin
             "cjpalhdlnbpafiamejdnhcphjbkeiagm"
