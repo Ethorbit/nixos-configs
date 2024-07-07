e9e071693a603432e30975016906a8ebab0949fb
Ethorbit <Ethorbit@protonmail.com>
2024-07-06 21:47:14 -0700

    Fix polybar powermenu

--- nixos/home-manager/polybar/modules/powermenu.nix
+++ nixos/home-manager/polybar/modules/powermenu.nix
@@ -24,33 +24,33 @@
             #menu-0-2-exec = "menu-open-5";
             menu-0-2 = "suspend";
             menu-0-2-exec = "menu-open-3";
-            menu-0-3 = "turn off monitors";
-            menu-0-3-exec = "menu-open-4";
+            #menu-0-3 = "turn off monitors";
+            #menu-0-3-exec = "menu-open-4";
 
             menu-1-0 = "cancel";
             menu-1-0-exec = "menu-open-0";
             menu-1-1 = "reboot";
-            menu-1-1-exec = "systemctl reboot";
+            menu-1-1-exec = "${config.system.path}/bin/reboot || sudo ${config.system.path}/bin/reboot";
 
             menu-2-0 = "power off";
-            menu-2-0-exec = "shutdown -h now";
+            menu-2-0-exec = "${config.system.path}/bin/shutdown -h now || sudo ${config.system.path}/bin/shutdown -h now";
             menu-2-1 = "cancel";
             menu-2-1-exec = "menu-open-0";
 
             menu-3-0 = "suspend";
-            menu-3-0-exec = "systemctl suspend";
+            menu-3-0-exec = "${config.system.path}/bin/systemctl suspend || sudo ${config.system.path}/bin/systemctl suspend";
             menu-3-1 = "cancel";
             menu-3-1-exec = "menu-open-0";
+#
+            #menu-4-0 = "turn off monitors";
+            #menu-4-0-exec = "${config.system.path}/bin/xset dpms force off || sudo ${config.system.path}/bin/xset dpms force off";
+            #menu-4-1 = "cancel";
+            #menu-4-1-exec = "menu-open-0";
 
-            menu-4-0 = "turn off monitors";
-            menu-4-0-exec = "xset dpms force off";
-            menu-4-1 = "cancel";
-            menu-4-1-exec = "menu-open-0";
-
-            menu-5-0 = "log out";
-            menu-5-0-exec = "systemctl restart lightdm";
-            menu-5-1 = "cancel";
-            menu-5-1-exec = "menu-open-0";
+            #menu-5-0 = "log out";
+            #menu-5-0-exec = "systemctl restart lightdm";
+            #menu-5-1 = "cancel";
+            #menu-5-1-exec = "menu-open-0";
         };
     };
 }
--- nixos/sudo.nix
+++ nixos/sudo.nix
@@ -7,7 +7,7 @@
             commands = [
                 { command = "${config.system.path}/bin/poweroff"; options = [ "NOPASSWD" ]; }
                 { command = "${config.system.path}/bin/reboot"; options = [ "NOPASSWD" ]; }
-                { command = "${config.system.path}/bin/suspend"; options = [ "NOPASSWD" ]; }
+                { command = "${config.system.path}/bin/systemctl suspend"; options = [ "NOPASSWD" ]; }
                 { command = "${config.system.path}/bin/shutdown"; options = [ "NOPASSWD" ]; }
                 { command = "${config.system.path}/bin/halt"; options = [ "NOPASSWD" ]; }
             ];
