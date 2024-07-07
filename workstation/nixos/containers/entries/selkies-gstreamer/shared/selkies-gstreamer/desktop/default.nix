1b1dc991ae38453a6780a341ad5f0b170561c72a
Ethorbit <Ethorbit@protonmail.com>
2024-07-06 18:14:19 -0700

    Fix Picom for selkies gstreamer containers
    Earlier the Picom default backend changed to glx because its default xrender performance was absolutely terrible (and rather than abandon Picom or copy paste glx in all my systems, I decided it should be the new default). Selkies Gstreamer uses VirtualGL for allowing isolated desktops to share the GPU, but when Picom is run with VirtualGL with the glx backend, it crashes at startup. The default container desktop config sets Picom's backend to xrender so that Picom will continue to work correctly.

--- workstation/nixos/containers/entries/selkies-gstreamer/shared/selkies-gstreamer/desktop/default.nix
+++ workstation/nixos/containers/entries/selkies-gstreamer/shared/selkies-gstreamer/desktop/default.nix
@@ -14,4 +14,9 @@
     # Selkies Gstreamer doesn't forward the windows key even in fullscreen, so we need to change it.
     # The browser connecting will need to have its shortcuts disabled to be able to use all the i3 keymaps.
     ethorbit.home-manager.i3.keys.mod = "${config.ethorbit.home-manager.i3.keys.alt}";
+
+    # Picom's defaults aren't compatible with Selkies Gstreamer's usage of VirtualGL
+    # We will need to change some properties so that picom won't crash at startup.
+    ethorbit.home-manager.picom.backend = "xrender";
+    #ethorbit.home-manager.picom.vSync = false;
 }
