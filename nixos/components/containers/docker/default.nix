aa6591e008cd2f3ec134b17b7fd21b44b72bae36
Ethorbit <Ethorbit@protonmail.com>
2024-08-24 20:07:54 -0700

    Fix nvidia-container-toolkit

--- nixos/components/containers/docker/default.nix
+++ nixos/components/containers/docker/default.nix
@@ -1,5 +1,7 @@
 { config, pkgs, lib, ... }:
 
+with lib;
+
 {
     imports = [
         ../.
@@ -13,7 +15,10 @@
         docker = {
             enable = true;
             enableOnBoot = true;
-            #enableNvidia = with lib; mkIf (config.hardware.nvidia.package != types.unspecified) true;
+
+            # trace: warning: You have set virtualisation.docker.enableNvidia. This option is deprecated, please set hardware.nvidia-container-toolkit.enable instead.
+            # ^ yeah so nvidia-container-toolkit DOES NOT work if the "new" option is used, so I'm still using this until things change..
+            enableNvidia = with lib; mkIf (config.hardware.nvidia.package != types.unspecified) true;
         };
 
         oci-containers = {
@@ -21,7 +26,7 @@
         };
     };
 
-    hardware.nvidia-container-toolkit.enable = with lib; mkIf (config.hardware.nvidia.package != types.unspecified) true;
+    hardware.nvidia-container-toolkit.enable = mkIf (config.hardware.nvidia.package != types.unspecified) true;
 
     users.users."${config.ethorbit.users.primary.username}" = {
         extraGroups = [ "docker" ];
