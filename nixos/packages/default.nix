267ea869e38bbb4e37cd921cc3995ba0d9289b24
Ethorbit <Ethorbit@protonmail.com>
2024-09-27 01:54:28 -0700

    Move packages.nix into packages/

---
+++ nixos/packages/default.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+    environment.systemPackages = with pkgs; [
+        bash
+        zsh
+        screen
+        killall
+        file
+        iotop
+        usbutils
+        pciutils
+        lm_sensors
+        lsof
+        restic
+        unzip
+        unrar
+        p7zip
+        whois
+        wget
+        git
+        sudo
+        vim
+    ];
+}
