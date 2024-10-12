d3e395e027e3cd3679685a6983c66c89d5a81994
Ethorbit <Ethorbit@protonmail.com>
2024-10-11 20:55:21 -0700

    Fix workstation steam i3 rules

--- workstation/nixos/home-manager/i3.nix
+++ workstation/nixos/home-manager/i3.nix
@@ -15,7 +15,9 @@
     home-manager.sharedModules = [ {
         home.file.".config/i3/config_system".text = ''
             # Second monitor will be used for casual gaming
-            for_window [class="steam|steamwebhelper"] move to workspace b1
+            for_window [class="steam"] move to workspace b1
+            for_window [class="steam" title="^(?!Steam|Friends List).*$"] floating enable
+            for_window [class="steam" title="Friends List"] focus, move right, move right, move right, move right, move right, move right, move right, resize set width 20 ppt
             for_window [class="steam_app_*"] move to workspace b1
         '';
     } ];
