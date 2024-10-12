66c631cef37543711fd8b06389fb5d50c1a77483
Ethorbit <Ethorbit@protonmail.com>
2024-10-12 01:09:21 -0700

    Fix Flatpak using wrong cursor

--- nixos/home-manager/flatpak/default.nix
+++ nixos/home-manager/flatpak/default.nix
@@ -1,11 +1,11 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
-    # Seems nixos flatpak remotes
-    # also adds to the user
-    # so there's currently
-    # nothing to do here..
-    services.flatpak = {
-
-    };
+    # Because apparently setting context in services.flatpak doesn't do anything :/
+    config.home-manager.sharedModules = [ {
+        home.file.".local/share/flatpak/overrides/global".text = ''
+          [Context]
+          filesystems=/run/current-system/sw/share/X11/fonts:ro;/nix/store:ro
+        '';
+    } ];
 }
