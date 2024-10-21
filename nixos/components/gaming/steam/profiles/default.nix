38f45a3d99d5372d555b7f7f1574bcc06607c208
Ethorbit <Ethorbit@protonmail.com>
2024-10-20 23:19:33 -0700

    Disable nice 10 for steam

--- nixos/components/gaming/steam/profiles/default.nix
+++ nixos/components/gaming/steam/profiles/default.nix
@@ -7,14 +7,23 @@
     #
     # We can set its nice level to give it
     # less CPU priority over other processes
-    services.ananicy.extraRules = [
-        {
-            name = "steam";
-            nice = 10;
-        }
-        {
-            name = "steamwebhelper";
-            nice = 10;
-        }
-    ];
+    #
+    # services.ananicy.extraRules = [
+    #     {
+    #         name = "steam";
+    #         nice = 10;
+    #     }
+    #     {
+    #         name = "steamwebhelper";
+    #         nice = 10;
+    #     }
+    # ];
+    #
+    # ^ Commented out because it's making processes
+    # (such as games) launched by steam get their
+    # niceness increased, reducing game performance.
+    #
+    # As a workaround, you can set download limits to
+    # decrease the amount of compression thus
+    # less lag from downloads.
 }
