528c000c40591e3895824ecf6c3c77af2d3657a0
Ethorbit <Ethorbit@protonmail.com>
2024-11-04 21:41:13 -0800

    Improve restic games comment

--- workstation/nixos/services/restic.nix
+++ workstation/nixos/services/restic.nix
@@ -102,8 +102,8 @@
                     OnCalendar = "daily";
                     Persistent = true;
                 };
-                # Only backup game saves and common config extensions
-                # to save on space
+                # Only backup game saves and files with common
+                # config file extensions to save on space
                 dynamicFilesFrom = ''
                     (find /mnt/games -type d -name "My Games" ;\
                     find /mnt/games -type d -name "*Saves*" ;\
