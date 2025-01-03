3eeffe71374ffe3bb1312761674d50f69b11e6cb
Ethorbit <Ethorbit@protonmail.com>
2025-01-02 16:13:11 -0800

    Set static ip for nzc dev

--- nzc/nixos/profiles/dev/networking.nix
+++ nzc/nixos/profiles/dev/networking.nix
@@ -1,6 +1,10 @@
 { config, ... }:
 
 {
+    systemd.network.networks."eth0" = {
+        address = [ "192.168.254.222/24" ];
+    };
+
     environment.etc."resolv.conf".text = ''
     # Created with environment.etc.'resolv.conf' NixOS option
     nameserver 127.0.0.1
