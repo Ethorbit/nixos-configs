2dbab281ad04f3229312109d9cf35d7da97ee572
Ethorbit <Ethorbit@protonmail.com>
2025-01-26 09:42:33 -0800

    i3, Remove xidlehook and xautolock comment
    They were originally configured because DPMS Standby stopped working (which I assumed was just a NVIDIA issue), and I needed to find a substitute quickly. Eventually I figured out it was actually Steam that was keeping the system active at all times.

--- nixos/components/window-manager/profiles/i3/services.nix
+++ nixos/components/window-manager/profiles/i3/services.nix
@@ -5,24 +5,6 @@ with lib;
 {
     services.xserver.windowManager.i3.enable = true;
 
-    # Doesn't work. We'll add our personal implementation of something that DOES
-    #services.xserver.xautolock = {
-    #    enable = true;
-    #    time = mkDefault 15;
-    #    locker = config.ethorbit.home-manager.i3.scripts.lock.outPath;
-    #    nowlocker = config.ethorbit.home-manager.i3.scripts.lock.outPath;
-    #};
-    ethorbit.services.xidlehook = {
-        enable = mkDefault true;
-        timers = [
-            {
-                config = {
-                    command = mkDefault config.ethorbit.home-manager.i3.scripts.lock.outPath;
-                };
-            }
-        ];
-    };
-
     programs.xss-lock = {
         enable = mkDefault true;
         lockerCommand = mkDefault config.ethorbit.home-manager.i3.scripts.lock.outPath;
