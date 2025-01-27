316c76b4e5f668124c19432766483e58403d7609
Ethorbit <Ethorbit@protonmail.com>
2025-01-27 03:25:50 -0800

    Workstation, disable PrismLauncher IPC sharing

--- workstation/nixos/home-manager/flatpak/minecraft.nix
+++ workstation/nixos/home-manager/flatpak/minecraft.nix
@@ -7,8 +7,14 @@ in
     home-manager.users.${config.ethorbit.users.primary.username} = {
         services.flatpak = {
             overrides = {
-                "${id}".Context = {
-                    filesystems = [ "/mnt/games/PrismLauncher" ];
+                "${id}" = {
+                    Context = {
+                        filesystems = [ "/mnt/games/PrismLauncher" ];
+
+                        # It can still do inter-process communication with its own container processes.
+                        # There's really no need for it to need this for my use case
+                        shared = "!ipc";
+                    };
                 };
             };
         };
