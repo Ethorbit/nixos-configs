86653308cf617839b5f02c8f17e3765183dc4e73
Ethorbit <Ethorbit@protonmail.com>
2025-02-10 12:46:37 -0800

    Enable Sysrq reboot for all systems

--- nixos/bootloader.nix
+++ nixos/bootloader.nix
@@ -1,9 +1,11 @@
 { config, lib, ... }:
 
 {
-    # Always have boot verbosity
-    boot = {
-        consoleLogLevel = lib.mkDefault 3;
-        initrd.verbose = lib.mkDefault true;
+    boot = with lib; {
+        # Always have boot verbosity
+        consoleLogLevel = mkDefault 3;
+        initrd.verbose = mkDefault true;
+        # Enable Sysrq REISUB
+        kernel.sysctl."kernel.sysrq" = mkDefault 502;
     };
 }
