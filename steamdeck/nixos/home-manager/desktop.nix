96c6bd9bb3577a214543cde7b86b76be899a1ec6
Ethorbit <Ethorbit@protonmail.com>
2025-02-15 00:25:47 -0800

    steamdeck Desktop, autostart Steam

--- steamdeck/nixos/home-manager/desktop.nix
+++ steamdeck/nixos/home-manager/desktop.nix
@@ -1,8 +1,8 @@
 { config, ... }:
 
 {
-    # Rotate XFCE desktop
     home-manager.sharedModules = [ {
+        # Rotate XFCE desktop
         xfconf.settings = {
             displays = {
                 "Rotation" = 0;
@@ -10,5 +10,21 @@
                 "Default/eDP-1/Reflection" = "0";
             };
         };
+
+        # Make XFCE auto start Steam (so native controls work)
+        home.file.".config/autostart/Steam.desktop".text = ''
+            [Desktop Entry]
+            Encoding=UTF-8
+            Version=0.9.4
+            Type=Application
+            Name=Steam
+            Comment=needed for inputs
+            Exec=steam
+            OnlyShowIn=XFCE;
+            RunHook=0
+            StartupNotify=false
+            Terminal=false
+            Hidden=false
+        '';
     } ];
 }
