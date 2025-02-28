d88c9b0181dfee7932db9d054be4f659bbc7dcbd
Ethorbit <Ethorbit@protonmail.com>
2025-02-28 09:39:28 -0800

    Steamdeck, make Desktop Mode the default
    Steam has a tendancy to do nasty imperative + mutable stuff such as auto-updating itself, changing its files at runtime, potentially breaking itself. By having Game Mode enabled as the default, you're putting yourself at the mercy of Steam to let you use your system, and risk getting locked out by a simple mistake made by a Valve employee. This change sets Desktop Mode to the default so that system usability is never delayed, and Steam can go ahead and break itself in the background, where nobody cares.

--- steamdeck/nixos/services.nix
+++ steamdeck/nixos/services.nix
@@ -1,6 +1,20 @@
 { config, ... }:
 
 {
+    # Set SteamOS session preference to Desktop at startup
+    # This way we're not dependent on Steam
+    # to use the system.
+    #
+    # This is useful if Steam decides to auto-update and take ages
+    systemd.services."steamos-force-desktop-mode" = {
+        wantedBy = [ "graphical.target" ];
+        before = [ "greetd.service" ];
+        script = ''
+            home_dir=$(${pkgs.getent}/bin/getent passwd "${config.ethorbit.users.primary.username}" | cut -d: -f6)
+            echo "${config.jovian.steam.desktopSession}" > "$home_dir/.local/state/steamos-session-select"
+        '';
+    };
+
     services.btrfs.autoScrub.enable = true;
 
     services.openssh = {
