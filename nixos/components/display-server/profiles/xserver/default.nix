0d77613794009a3aebd579817860f7e351080831
Ethorbit <Ethorbit@protonmail.com>
2025-04-16 23:14:18 -0700

    Filter bluelight for all X systems
    The defaults are to match my Android settings so that both my Android phone and all my systems filter at the same time. A system can change this, but ideally I want all my systems to synchronize, so I've enforced the defaults in the global xserver component

--- nixos/components/display-server/profiles/xserver/default.nix
+++ nixos/components/display-server/profiles/xserver/default.nix
@@ -3,6 +3,7 @@
 {
     imports = [
         ../..
+        ./redshift.nix
     ];
 
     services.xserver = {
---
+++ nixos/components/display-server/profiles/xserver/redshift.nix
@@ -0,0 +1,45 @@
+# 11pm, night light mode
+# 7am, normal blue light
+# Transitions immediately
+# Mimics my setup on Android with its "Night Light"
+
+{ config, lib, pkgs, ... }:
+
+{
+    systemd.user = {
+        services = {
+            "redshift-daytime" = {
+                script = lib.mkDefault ''
+                ${pkgs.redshift}/bin/redshift -x
+                '';
+            };
+
+            "redshift-nighttime" = {
+                script = lib.mkDefault ''
+                ${pkgs.redshift}/bin/redshift -x
+                ${pkgs.redshift}/bin/redshift -O 2000
+                '';
+            };
+        };
+
+        timers = {
+            "redshift-daytime" = {
+                enable = true;
+                timerConfig = {
+                    OnCalendar = lib.mkDefault "07:00";
+                    Persistent = true;
+                };
+                wantedBy = [ "timers.target" ];
+            };
+
+            "redshift-nighttime" = {
+                enable = true;
+                timerConfig = {
+                    OnCalendar = lib.mkDefault "23:00";
+                    Persistent = true;
+                };
+                wantedBy = [ "timers.target" ];
+            };
+        };
+    };
+}
