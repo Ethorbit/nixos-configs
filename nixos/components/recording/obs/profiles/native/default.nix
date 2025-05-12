9dd1174617fa78293f5c134738ab762627ec7353
Ethorbit <Ethorbit@protonmail.com>
2025-05-12 16:48:38 -0700

    Fix OBS component module

--- nixos/components/recording/obs/profiles/native/default.nix
+++ nixos/components/recording/obs/profiles/native/default.nix
@@ -1,7 +1,21 @@
-{ config, ... }:
+{ config, lib, pkgs, ... }:
+
+with lib;
 
 {
     imports = [
         ../../.
+        ./home-manager.nix
     ];
+
+    programs.obs-studio = {
+        enable = true;
+        plugins = with pkgs.obs-studio-plugins; [
+            obs-vkcapture
+        ] ++ mkIf services.pipewire.enable [
+            obs-pipewire-audio-capture
+        ] ++ mkIf hardware.pulseaudio.enable [
+            # TODO: add my NixOS port of pulseaudio-audio-capture
+        ];
+    };
 }
