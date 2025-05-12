04f74829d5c503c6cd146b9eae7ea699b348c6bc
Ethorbit <Ethorbit@protonmail.com>
2025-05-12 01:23:49 -0700

    Workstation, make OBS Replay active on launch

--- workstation/nixos/flatpak/obs.nix
+++ workstation/nixos/flatpak/obs.nix
@@ -1,6 +1,36 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
+let
+    flatpakCommand = pkgs.writeShellScript "command.sh" ''
+        obs --startreplaybuffer --minimize-to-tray
+    '';
+
+    desktopScript = pkgs.writeShellScript "script.sh" ''
+        flatpak run --command="${flatpakCommand.outPath}" com.obsproject.Studio
+    '';
+in
 {
+    # This will make sure OBS starts in the background with Replay ON
+    home-manager.sharedModules = [ {
+        xdg.desktopEntries = {
+            "com.obsproject.Studio2" = {
+                name = "OBS Studio (Custom)";
+                icon = "com.obsproject.Studio";
+                genericName = "Streaming/Recording Software";
+                exec = desktopScript.outPath;
+                categories = [
+                    "AudioVideo"
+                    "Recorder"
+                ];
+                startupNotify = true;
+            };
+        };
+        # LOL if you want to actually hide the original desktop entry, it cannot be done declaratively..
+        # So to avoid original OBS from appearing on top, you have to mutably create the desktop entry in your home,
+        # hide it with NoDisplay=true so that this custom entry is the only one that shows.
+        #home.file."~/.local/share/applications/com.obsproject.Studio.desktop".source = desktopFile;
+    } ];
+
     services.flatpak = {
         enable = true;
         packages = [
