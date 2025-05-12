b868644435249ee2ee08a9f695d54d6ba62c8db9
Ethorbit <Ethorbit@protonmail.com>
2025-05-12 15:43:48 -0700

    Make component module for OBS

---
+++ nixos/components/recording/obs/profiles/flatpak/default.nix
@@ -0,0 +1,30 @@
+{ config, ... }:
+
+let
+     cfg = config.ethorbit.components.recording.obs.flatpak;
+in
+{
+    imports = [
+        ../../.
+        ./options.nix
+        ./home-manager.nix
+    ];
+
+    services.flatpak = {
+         enable = true;
+         packages = [
+             {
+                 appId = "${cfg.appIds.obs}";
+                 origin = "flathub";
+             }
+             {
+                 appId = "${cfg.appIds.obsVkCapture}";
+                 origin = "flathub";
+             }
+             {
+                 appId = "${cfg.appIds.obsVkCapturePlugin}";
+                 origin = "flathub";
+             }
+         ];
+     };
+}
---
+++ nixos/components/recording/obs/profiles/flatpak/options.nix
@@ -0,0 +1,24 @@
+{ config, lib, ... }:
+
+with lib;
+
+{
+    options.ethorbit.components.recording.obs.flatpak = {
+        appIds = {
+            obs = mkOption {
+                type = types.str;
+                default = "com.obsproject.Studio";
+            };
+
+            obsVkCapture = mkOption {
+                type = types.str;
+                default = "org.freedesktop.Platform.VulkanLayer.OBSVkCapture/x86_64/24.08";
+            };
+
+            obsVkCapturePlugin = mkOption {
+                type = types.str;
+                default = "com.obsproject.Studio.Plugin.OBSVkCapture";
+            };
+        };
+    };
+}
