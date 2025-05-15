6726315dcdf8451e2862c0d0dde81bc11521177f
Ethorbit <Ethorbit@protonmail.com>
2025-05-15 14:19:50 -0700

    OpenGL component, fix vulkan ICD errors

--- nixos/components/graphics-drivers/opengl/before-24.11.nix
+++ nixos/components/graphics-drivers/opengl/before-24.11.nix
@@ -4,7 +4,9 @@
     # The option definition `hardware.opengl.driSupport' no longer has any effect; please remove it.
     config = lib.mkIf (config.system.nixos.release < "24.11") {
         hardware.opengl = {
+            enable = true;
             driSupport = true;
+            driSupport32Bit = lib.mkDefault true;
         };
     };
 }
