9274ef8355041782bebead8938f26316541dbc59
Ethorbit <Ethorbit@protonmail.com>
2025-06-20 13:34:50 -0700

    Use nix-packages watch-xfce-xfconf

--- nixos/components/desktop-environment/profiles/xfce/packages.nix
+++ nixos/components/desktop-environment/profiles/xfce/packages.nix
@@ -1,12 +1,12 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-    imports = [
-        ../../../../packages/python/watch-xfce-xfconf
-    ];
-
-    environment.systemPackages = with pkgs; with xfce; [
-        config.ethorbit.pkgs.python.watch-xfce-xfconf
+    environment.systemPackages = 
+        with pkgs;
+        with xfce;
+        with ethorbit.xfce;
+    [
+        watch-xfce-xfconf
         xfce4-whiskermenu-plugin
         xfce4-docklike-plugin
         xfce4-timer-plugin
