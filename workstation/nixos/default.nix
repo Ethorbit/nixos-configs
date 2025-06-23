1501dbecffe0e43750546d43de8d5cf8c8929c43
Ethorbit <Ethorbit@protonmail.com>
2025-06-22 19:26:49 -0700

    Use new nix-packages module namespace

--- workstation/nixos/default.nix
+++ workstation/nixos/default.nix
@@ -40,5 +40,5 @@
 
     ethorbit.graphics.nvidia.proprietary.selectedPackage = config.boot.kernelPackages.nvidiaPackages.stable;
 
-    programs.termdown-wrapper.soundPath = "/home/${config.ethorbit.users.primary.username}/Documents/timer.opus";
+    ethorbit.programs.termdown-wrapper.soundPath = "/home/${config.ethorbit.users.primary.username}/Documents/timer.opus";
 }
