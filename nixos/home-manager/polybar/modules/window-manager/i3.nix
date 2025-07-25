2c6520560186db52844a052221734cf73167eb48
Ethorbit <Ethorbit@protonmail.com>
2025-07-25 14:11:23 -0700

    Fix Polybar syntax errors

--- nixos/home-manager/polybar/modules/window-manager/i3.nix
+++ nixos/home-manager/polybar/modules/window-manager/i3.nix
@@ -1,9 +1,17 @@
 { config, ... }:
 
+let
+    cfg = config.ethorbit.polybar;
+    self = {
+        label-focused-background = cfg.colors.background-alt;
+        label-focused-underline = "#ffffff";
+        label-focused-padding = 2;
+    };
+in
 {
     home-manager.sharedModules = [ {
         services.polybar.config = {
-            "module/i3" = {
+            "module/i3" = self // {
                 type = "internal/i3";
                 format = "<label-state> <label-mode>";
                 index-sort = true;
@@ -14,14 +22,11 @@
 
                 label-mode-padding = 2;
                 label-mode-foreground = "#000";
-                label-mode-background = config.ethorbit.polybar.colors.primary;
+                label-mode-background = cfg.colors.primary;
 
                 # focused = Active workspace on focused monitor
                 label-focused = "%name%";
-                label-focused-background = config.ethorbit.polybar.colors.background-alt;
-                label-focused-underline= "#ffffff";
                 #${colors.primary}
-                label-focused-padding = 2;
 
                 # unfocused = Inactive workspace on any monitor
                 label-unfocused = "%name%";
@@ -29,13 +34,13 @@
 
                 # visible = Active workspace on unfocused monitor
                 label-visible = "%name%";
-                label-visible-background = "$${self.label-focused-background}";
-                label-visible-underline = "$${self.label-focused-underline}";
-                label-visible-padding = "$${self.label-focused-padding}";
+                label-visible-background = self.label-focused-background;
+                label-visible-underline = self.label-focused-underline;
+                label-visible-padding = self.label-focused-padding;
 
                 # urgent = Workspace with urgency hint set
                 label-urgent = "%name%";
-                label-urgent-background = config.ethorbit.polybar.colors.alert;
+                label-urgent-background = cfg.colors.alert;
                 label-urgent-padding = 2;
 
                 # Separator in between workspaces
