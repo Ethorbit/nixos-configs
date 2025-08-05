3cabe127a4a3db9576aeacb01e253d3fce717504
Ethorbit <Ethorbit@protonmail.com>
2025-08-05 01:28:55 -0700

    Workstation, set root password

--- workstation/nixos/users.nix
+++ workstation/nixos/users.nix
@@ -15,17 +15,14 @@
                 extraGroups = [ "wheel" "libvirtd" "docker" "container" "audio" "video" "input" ];
             };
 
+            # just needed for emergency boots.
+            "root".hashedPassword = config.users.users."workstation".hashedPasswordFile;
+
             "builder" = {
                 isNormalUser = true;
                 group = "builder";
                 hashedPasswordFile = config.users.users."workstation".hashedPasswordFile;
             };
-        };
-
-        # workstation uses sudo and can do nixos-container root-login
-        # to access root on containers
-        users."root" = {
-            hashedPassword = "!";
         };
     };
 
