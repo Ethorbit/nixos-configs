236944370231c80dc7348b275c68947377306824
Ethorbit <Ethorbit@protonmail.com>
2025-08-19 16:55:37 -0700

    Use hm xfconf

--- nixos/components/desktop-environment/profiles/xfce/default.nix
+++ nixos/components/desktop-environment/profiles/xfce/default.nix
@@ -1,12 +1,12 @@
-{ config, ... }:
+{ homeModules, ... }:
 
 {
     imports = [
         ./packages.nix
         ../..
-        ../../../../home-manager/xfconf
     ];
 
+    home-manager.sharedModules = [ homeModules.xfconf ];
     programs.xfconf.enable = true;
     services.xserver.desktopManager.xfce.enable = true;
 }
