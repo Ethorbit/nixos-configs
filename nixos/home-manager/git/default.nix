f016a4901c030a6b69bcf3c2d8414ae6a2bebc8f
Ethorbit <Ethorbit@protonmail.com>
2025-08-19 23:37:58 -0700

    Add hm git

--- nixos/home-manager/git/default.nix
+++ nixos/home-manager/git/default.nix
@@ -1,42 +1,13 @@
-{ config, lib, ... }:
+{ homeModules, ... }:
 
 {
-    options.ethorbit.home-manager.git = with lib; {
-        user = {
-            email = mkOption {
-                description = "Your email";
-                default = "Ethorbit@protonmail.com";
+    home-manager.sharedModules = 
+     with homeModules; [
+            git
+        ] ++ [ {
+            ethorbit.home-manager.git = {
+                user.email = "Ethorbit@protonmail.com";
+                user.name = "Ethorbit";
             };
-
-            name = mkOption {
-                description = "Your username";
-                default = "Ethorbit";
-            };
-        };
-    };
-
-    config = {
-        home-manager.users.${config.ethorbit.users.primary.username} = {
-            programs.git = {
-                enable = true;
-            };
-
-            home.file.".gitconfig" = {
-                text = ''
-                [user]
-                    email = ${config.ethorbit.home-manager.git.user.email}
-                    name = ${config.ethorbit.home-manager.git.user.name}
-                [credential]
-                    helper = cache
-                [pull]
-                    ff = true
-                [filter "lfs"]
-                    process = git-lfs filter-process
-                    required = true
-                    clean = git-lfs clean -- %f
-                    smudge = git-lfs smudge -- %f
-                '';
-            };
-        };
-    };
+        } ];
 }
