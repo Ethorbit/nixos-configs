f358a0c69aced9192d64c4d6bb2182b6966f917e
Ethorbit <11908638+Ethorbit@users.noreply.github.com>
2025-08-29 14:43:17 -0700

    Disable kwallet for all systems
    This is a workaround for an issue caused by NixOS 25.05 where it has seemingly polluted all my systems with kwallet. Kwallet is causing browser launch delays and other issues. For now, I'm disabling it via a dotfile, but once I find the root cause, it'll actually be removed

--- nixos/home-manager/default.nix
+++ nixos/home-manager/default.nix
@@ -15,6 +15,16 @@
             shell
             htop
             xdg
-        ];
+        ] ++ [ {
+            # https://discourse.nixos.org/t/brave-browser-and-kde-wallet-on-25-05/64915
+            # NixOS 25.05 seems to have polluted my systems with kwallet
+            # and kwallet is delaying browser launch by ~60 secs
+            #
+            # Until I can find out the root cause:
+            home.file.".config/kwalletrc".text = ''
+                [Wallet]
+                Enabled=false
+            '';
+        } ];
     };
 }
--- workstation/packages/default.nix
+++ workstation/packages/default.nix
@@ -8,40 +8,19 @@
     programs.adb.enable = true;
 
     environment.systemPackages = with pkgs; [
-        distrobox
-        flatpak-builder
-
-        coturn
         neofetch
 
         libreoffice-qt
-
-        virtiofsd
-        virt-viewer
 
         keepassxc
 
         ethorbit.filen-cli
         filezilla
         sshfs
-        ntfs3g
 
         yt-dlp
         ethorbit.yt-dlp-wrapper
 
-        # steals your data
-        #firefox
-        # can't play YouTube correctly
-        #(symlinkJoin {
-        #    name = "ungoogled-chromium-wrapped";
-        #    paths = [
-        #        ungoogled-chromium
-        #    ];
-        #    buildInputs = [ makeWrapper ];
-        #    postBuild = ''
-        #        ${config.ethorbit.components.web-browsing.chromium.wrappers.videoEncoding}
-        #    '';
-        #})
         # so far, so good...
         (symlinkJoin {
             name = "brave-wrapped";
@@ -57,9 +36,6 @@
         audacity
         blender
         lmms
-        #godot_4-mono
-        #dotnet-sdk
-        #godot_4
         krita
         ethorbit.taggui  
     ];
