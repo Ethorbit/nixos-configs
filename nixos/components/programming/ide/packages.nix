192f7bef91caf03c4c6aa287c1b5158721906ee9
Ethorbit <11908638+Ethorbit@users.noreply.github.com>
2025-08-31 17:10:46 -0700

    Update hm modules flake

--- nixos/components/programming/ide/packages.nix
+++ nixos/components/programming/ide/packages.nix
@@ -1,32 +1,9 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
     environment.systemPackages = with pkgs; [
-        ranger
-        trash-cli
         git-lfs
         qt6.full
-        nil
-        unstable.mdx-language-server
         python311Packages.grip
-        (python3.withPackages (p: (with p; [
-            python-lsp-server
-            flake8
-            pycodestyle
-            pyflakes
-            pylint
-            pylsp-mypy
-            pyls-isort
-        ])))
-        nodePackages.eslint
-        # error: vscode-json-languageserver-bin has been removed since the upstream repository is archived; consider using vscode-langservers-extracted instead.
-        #nodePackages.vscode-json-languageserver-bin
-        #nodePackages.vscode-html-languageserver-bin
-        vscode-langservers-extracted
-        nodePackages.typescript-language-server
-        unstable.nodePackages.svelte-language-server
-        lua-language-server
-        dockerfile-language-server-nodejs
-        docker-compose-language-service
     ];
 }
