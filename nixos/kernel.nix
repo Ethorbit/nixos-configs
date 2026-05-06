57ab9ad655e9c111a54f48384af0fe132f86efbf
Ethorbit <11908638+Ethorbit@users.noreply.github.com>
2026-05-06 16:27:49 -0700

    Kernel upgrade: protect against CopyFail

--- nixos/kernel.nix
+++ nixos/kernel.nix
@@ -1,7 +1,10 @@
+# Copyfail CVE-2026-31431 root escalation defense
+# https://discourse.nixos.org/t/is-nixos-affected-by-copy-fail-edit-yes-it-is/77317
+
+{ pkgs, lib, ... }:
+
 {
-    # Copyfail vulnerability protection
-    # https://discourse.nixos.org/t/is-nixos-affected-by-copy-fail-edit-yes-it-is/77317
-    # Protect against the copy fail exploit by blacklisting the affected kernel modules
+    # Protect against the exploit by blacklisting the affected kernel modules
     boot.blacklistedKernelModules = [
         "af_alg"
         "algif_hash"
@@ -9,4 +12,9 @@
         "algif_rng"
         "algif_aead"
     ];
+
+    # Upgrade to a safe kernel
+    boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.18.22") (
+        lib.mkDefault pkgs.linuxPackages_6_18
+    );  
 }
