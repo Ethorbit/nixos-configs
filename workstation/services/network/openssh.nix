d8d3d949abf9851945d69b0d3a33786bffc0eb92
Ethorbit <11908638+Ethorbit@users.noreply.github.com>
2026-05-07 03:13:08 -0700

    SSH listen only to host

--- workstation/services/network/openssh.nix
+++ workstation/services/network/openssh.nix
@@ -3,6 +3,14 @@
 {
     services.openssh = {
         enable = true;
+
+        listenAddresses = [
+            {
+                addr = config.ethorbit.workstation.network.host.ip;
+                port = 22;
+            }
+        ];
+
         settings = {
             PermitRootLogin = "no";
         };
