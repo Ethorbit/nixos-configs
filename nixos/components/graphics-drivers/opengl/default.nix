9f3ecb15fda77f862efaaeeb1cc2d360dfab235b
Ethorbit <11908638+Ethorbit@users.noreply.github.com>
2026-06-12 01:08:30 -0700

    Remove outdated vulkan fix for graphics component

--- nixos/components/graphics-drivers/opengl/default.nix
+++ nixos/components/graphics-drivers/opengl/default.nix
@@ -12,12 +12,4 @@
             VDPAU_DRIVER = lib.mkDefault "va_gl";
         };
     };
-
-    # this fixes garbage ICD errors...
-    # https://discourse.nixos.org/t/nixos-issues-with-vulkan-after-upgrading-from-24-05-to-24-11/56949/8
-    # I don't think it's related to 24.11 though, pretty sure stupid me always had the problem and I only
-    # just found out by running vulkaninfo, lol
-    environment.sessionVariables.VK_ICD_FILENAMES =
-      "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json:"
-      + "/run/opengl-driver-32/share/vulkan/icd.d/nvidia_icd.i686.json";
 }
