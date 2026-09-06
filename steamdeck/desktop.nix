3a580aff8d2292572048bac745b2f81861908303
Ethorbit <11908638+Ethorbit@users.noreply.github.com>
2026-09-06 14:33:48 -0700

    Add Game Mode shortcut to steamdeck desktop

--- steamdeck/desktop.nix
+++ steamdeck/desktop.nix
@@ -1,5 +1,11 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
+let
+    gameModeScript = pkgs.writeShellScript "steamos-switch-to-game-mode" ''
+        /run/current-system/sw/bin/steamosctl switch-to-game-mode
+        xfce4-session-logout --logout --fast
+    '';
+in
 {
     imports = [
         ../nixos/components/display-server/profiles/xserver
@@ -8,4 +14,15 @@
 
     # Add 'startx' or else desktop mode won't work
     services.xserver.displayManager.startx.enable = true;
+
+    environment.systemPackages = [
+        (pkgs.makeDesktopItem {
+            name = "steamos-game-mode";
+            desktopName = "Switch to Game Mode";
+            comment = "Switch this SteamOS session back to Game Mode";
+            exec = "${gameModeScript}";
+            icon = "input-gaming";
+            categories = [ "System" "Game" ];
+        })
+    ];
 }
